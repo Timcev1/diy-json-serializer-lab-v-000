dd2bd650453b3894b21f4cd0cce5ffb012e7d13e
Timothy Cevallos <timcev@gmail.com>
2017-10-05 13:51:26 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,10 +4,10 @@ class ProductSerializer
     # start with the open brace to create a valid JSON object
     serialized_product = '{'
 
-    serialized_product += '"id": ' + post.id.to_s + ', '
-    serialized_product += '"title": "' + post.title + '", '
-    serialized_product += '"description": "' + post.description + '", '
-    
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"title": "' + product.title + '", '
+    serialized_product += '"description": "' + product.description + '", '
+
     serialized_product += '}'
   end
 end
