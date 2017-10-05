0c768e18003de86d159bf2e8cb55fd8b81ac31f7
Timothy Cevallos <timcev@gmail.com>
2017-10-05 13:51:14 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -7,6 +7,7 @@ class ProductSerializer
     serialized_product += '"id": ' + post.id.to_s + ', '
     serialized_product += '"title": "' + post.title + '", '
     serialized_product += '"description": "' + post.description + '", '
+    
     serialized_product += '}'
   end
 end
